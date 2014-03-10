4e45853ed53d684dd5fc8648aea429ef0b3a4ac0
Andrew Lane <andrew.lane@hobsons.com>
2014-03-09 22:56:28 -0400

    Adding a console app solution as the groundwork for how I'm going to solve this

---
+++ src/WordsWithBrutes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordsWithBrutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Words with Brutes!");
+        }
+    }
+}
