ae501e04e88872f3fff84e8e0da13c0f4e14c311
Andrew Lane <andrew.lane@hobsons.com>
2014-03-12 07:12:19 -0400

    * Adding a model for a played word

---
+++ src/WordsWithBrutes/Model/PlayedWord.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WordsWithBrutes.Model
+{
+    /// <summary>
+    /// Models a word that's played
+    /// </summary>
+    public class PlayedWord
+    {
+        public IEnumerable<PlayedTile> TilesPlayed { get; set; }
+    }
+}
