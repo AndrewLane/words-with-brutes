274d2e7e8e53be6d864f0566ba5f293c27fbcdaa
Andrew Lane <andrew.lane@hobsons.com>
2014-03-13 15:05:44 -0400

    Adding modeling for game state and potential play location

---
+++ src/WordsWithBrutes/Model/GameState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace WordsWithBrutes.Model
+{
+    /// <summary>
+    /// Models the state a given game or challenge is in at any time
+    /// </summary>
+    public class GameState
+    {
+        /// <summary>
+        /// The challenge that this game lives inside of
+        /// </summary>
+        public Challenge Challenge { get; set; }
+
+        /// <summary>
+        /// The words that have been played so far in this game
+        /// </summary>
+        public IEnumerable<PlayedWord> PlayedWords { get; set; }
+
+        /// <summary>
+        /// The points accumulated so far in the game
+        /// </summary>
+        public int PointsSoFar { get; set; }
+
+        /// <summary>
+        /// The current state of the rack
+        /// </summary>
+        public Rack CurrentRack { get; set; }
+
+        /// <summary>
+        /// Whether this game is "done"
+        /// </summary>
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ src/WordsWithBrutes/Model/PotentialPlayLocation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WordsWithBrutes.Model
+{
+    /// <summary>
+    /// Models a place on the board (with one more more tiles) where a new word can be played
+    /// </summary>
+    public class PotentialPlayLocation
+    {
+        /// <summary>
+        /// The collection of places where new tiles will be put to form the word(s)
+        /// </summary>
+        public IEnumerable<TileLocation> TileLocations { get; set; }
+    }
+}
