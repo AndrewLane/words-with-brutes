4b0c3e0960a4e195d479807d0fe5c05e9607189a
Andrew Lane <andrew.lane@hobsons.com>
2014-03-13 15:26:18 -0400

    Start adding some unit tests for the DetermineIfAStringIsAPlayableWord component (don't pass right now)

---
+++ src/WordsWithBrutes.Tests/Components/Impl/TestDetermineIfAStringIsAPlayableWord.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using WordsWithBrutes.Components.Impl;
+
+namespace WordsWithBrutes.Tests.Components.Impl
+{
+    /// <summary>
+    /// Tests everything that DetermineIfAStringIsAPlayableWord can do
+    /// </summary>
+    [TestFixture]
+    public class TestDetermineIfAStringIsAPlayableWord
+    {
+        /// <summary>
+        /// Verifies that single letters don't count as words
+        /// </summary>
+        [TestCase("a")]
+        [TestCase("A")]
+        [TestCase("z")]
+        [TestCase("Z")]
+        public void TestSingleLettersDontCountAsWords(string input)
+        {
+            new DetermineIfAStringIsAPlayableWord().IsAWord(input).Should().BeFalse();
+        }
+    }
+}
