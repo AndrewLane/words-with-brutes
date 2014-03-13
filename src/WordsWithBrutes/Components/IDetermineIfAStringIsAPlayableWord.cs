087b46c970e4853fbadcdf4552e1a343ca041412
Andrew Lane <andrew.lane@hobsons.com>
2014-03-13 15:14:38 -0400

    Adding a component that will determine if a given string is a word

---
+++ src/WordsWithBrutes/Components/IDetermineIfAStringIsAPlayableWord.cs
@@ -0,0 +1,13 @@
+﻿namespace WordsWithBrutes.Components
+{
+    /// <summary>
+    /// Checks words against a dictionary
+    /// </summary>
+    public interface IDetermineIfAStringIsAPlayableWord
+    {
+        /// <summary>
+        /// Determines if the given string is a word
+        /// </summary>
+        bool IsAWord(string input);
+    }
+}
---
+++ src/WordsWithBrutes/Components/Impl/DetermineIfAStringIsAPlayableWord.cs
@@ -0,0 +1,13 @@
+﻿namespace WordsWithBrutes.Components.Impl
+{
+    internal class DetermineIfAStringIsAPlayableWord : IDetermineIfAStringIsAPlayableWord
+    {
+        /// <summary>
+        /// Determines if the given string is a word
+        /// </summary>
+        public bool IsAWord(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
