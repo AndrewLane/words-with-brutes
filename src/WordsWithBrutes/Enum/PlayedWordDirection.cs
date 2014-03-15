2a28921132c4fbba8b37597ef937ab1905a95eb6
Andrew Lane <andrew.lane@hobsons.com>
2014-03-15 07:44:05 -0400

    Adding an enum that models the possible directions a word can be played in

---
+++ src/WordsWithBrutes/Enum/PlayedWordDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace WordsWithBrutes.Enum
+{
+    /// <summary>
+    /// Models the different directions a played word can be.  A single tile word is assumed to be horizontal.
+    /// </summary>
+    public enum WordDirection
+    {
+        Vertical,
+        Horizontal
+    }
+}
