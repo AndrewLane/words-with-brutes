564625a73dbe9bba88046bb4228422019a09b517
Andrew Lane <andrew.lane@hobsons.com>
2014-03-15 21:42:34 -0400

    Implementation for DeterminePotentialPlayLocationsForNextWord along with some elaborate and painstaking unit tests

---
+++ src/WordsWithBrutes.Tests/Components/Impl/TestDeterminePotentialPlayLocationsForNextWord.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using WordsWithBrutes.Components;
+using WordsWithBrutes.Components.Impl;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Tests.Components.Impl
+{
+    /// <summary>
+    /// Tests everything that DeterminePotentialPlayLocationsForNextWord can do
+    /// </summary>
+    [TestFixture]
+    public class TestDeterminePotentialPlayLocationsForNextWord
+    {
+        #region constants for easy-to-write tests
+        private const string SingleCell = "X";
+
+        const string TwoByTwoUpperLeftFree = @"
+ X
+XX";
+        const string TwoByTwoUpperRightFree = @"
+X
+XX";
+        const string TwoByTwoLowerLeftFree = @"
+XX
+ X";
+        const string TwoByTwoLowerRightFree = @"
+XX
+X ";
+        const string TwoByTwoTotallyFull = @"
+XX
+XX";
+        private const string ThreeByThreeWithAHole = @"
+XXX
+X X
+XXX";
+        private const string ThreeByThreeUpperLeftOnly = @"
+X  
+   
+   ";
+        private const string ThreeByThreeLeftColumnButNotUpperLeft = @"
+   
+X  
+X  ";
+        private const string ThreeByThreeTopRowButNotUpperLeft = @"
+ XX
+   
+   ";
+        private const string ThreeByThreeMiddleColumnButNotBottomRow = @"
+ X 
+ X 
+   ";
+        private const string ThreeByThreeMiddleRowButRightColumn = @"
+   
+XX 
+   ";
+
+        private const string ThreeByThreeLeftColumnMiddleRow = @"
+   
+X  
+   ";
+        private const string ThreeByThreeMiddleColumnTopRow = @"
+ X 
+   
+   ";
+        const string TwoByTwoUpperLeftOnly = @"
+X 
+  ";
+        const string TwoByTwoUpperRightOnly = @"
+ X
+  ";
+        const string TwoByTwoLowerLeftOnly = @"
+  
+X ";
+        const string TwoByTwoLowerRightOnly = @"
+  
+ X";
+        private const string ThreeByThreeJustTheMiddle = @"
+   
+ X 
+   ";
+        private const string FiveByFiveJustTheMiddle = @"
+     
+     
+  X  
+     
+     ";
+        private const string FiveByFiveWithBorderAndMiddle = @"
+XXXXX
+X   X
+X X X
+X   X
+XXXXX";
+        private const string FifteenByFifteenWithFiveLetterWordToStart = @"
+               
+               
+               
+               
+               
+               
+   XXXXX     
+               
+               
+               
+               
+               
+               
+               
+               ";
+
+
+        #endregion constants for easy-to-write tests
+
+
+        /// <summary>
+        /// Tests the GetPlacesToPlayNextWord method to make sure it returns the correct potential plays
+        /// </summary>
+        [TestCase(SingleCell, 1, 0)]
+        [TestCase(TwoByTwoTotallyFull, 1, 0)]
+        [TestCase(SingleCell, 7, 0)]
+        [TestCase(TwoByTwoTotallyFull, 7, 0)]
+        [TestCase(ThreeByThreeUpperLeftOnly, 1, 2, ThreeByThreeLeftColumnMiddleRow, ThreeByThreeMiddleColumnTopRow)]
+        [TestCase(ThreeByThreeUpperLeftOnly, 2, 6,
+            ThreeByThreeLeftColumnMiddleRow, ThreeByThreeMiddleColumnTopRow, ThreeByThreeLeftColumnButNotUpperLeft, 
+            ThreeByThreeTopRowButNotUpperLeft, ThreeByThreeMiddleColumnButNotBottomRow, ThreeByThreeMiddleRowButRightColumn)]
+        public void TestGetPlacesToPlayNextWordWithSingleExpectedMatch(string inputBoard, int lettersOnRack, int expectedPotentialPlays, params string[] expectedMatches)
+        {
+            //make sure there isn't a bug in the test specification
+            expectedMatches.Count().Should().Be(expectedPotentialPlays);
+
+            var inputMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(inputBoard);
+            var intputGameState = GetDummyGameStateFromOccupiedTiles(inputMultiArray, lettersOnRack);
+
+            //set up our ITransformGameStateIntoTwoDimensionalArray object to return our multi-dimensional array
+            var mockArrayTransformer = new Mock<ITransformGameStateIntoTwoDimensionalArray>(MockBehavior.Strict);
+            mockArrayTransformer.Setup(mock => mock.Transform(intputGameState)).Returns(inputMultiArray);
+
+            var objectUnderTest = new DeterminePotentialPlayLocationsForNextWord(mockArrayTransformer.Object);
+
+            //get all the places to play, and then make sure they match the expected
+            var placesToPlay = objectUnderTest.GetPlacesToPlayNextWord(intputGameState).ToList();
+            placesToPlay.Count().Should().Be(expectedPotentialPlays);
+            foreach (var expectedPlay in expectedMatches)
+            {
+                var expectedPlayMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(expectedPlay);
+                int matchesFound = placesToPlay.Count(placeToPlay => DoesPotentialPlayLocationMatch(expectedPlayMultiArray, placeToPlay));
+                matchesFound.Should().Be(1);
+            }
+        }
+
+        /// <summary>
+        /// Tests the GetPlacesToPlayNextWord method to make sure it returns the correct potential play in the case of only a single play being possible
+        /// </summary>        
+        [TestCase(TwoByTwoUpperLeftFree, 1, TwoByTwoUpperLeftOnly)]
+        [TestCase(TwoByTwoUpperRightFree, 1, TwoByTwoUpperRightOnly)]
+        [TestCase(TwoByTwoLowerLeftFree, 1, TwoByTwoLowerLeftOnly)]
+        [TestCase(TwoByTwoLowerRightFree, 1, TwoByTwoLowerRightOnly)]
+        [TestCase(ThreeByThreeWithAHole, 1, ThreeByThreeJustTheMiddle)]
+        [TestCase(TwoByTwoUpperLeftFree, 7, TwoByTwoUpperLeftOnly)]
+        [TestCase(TwoByTwoUpperRightFree, 7, TwoByTwoUpperRightOnly)]
+        [TestCase(TwoByTwoLowerLeftFree, 7, TwoByTwoLowerLeftOnly)]
+        [TestCase(TwoByTwoLowerRightFree, 7, TwoByTwoLowerRightOnly)]
+        [TestCase(ThreeByThreeWithAHole, 7, ThreeByThreeJustTheMiddle)]
+        public void TestGetPlacesToPlayNextWordWithSingleExpectedMatch(string inputBoard, int lettersOnRack, string expectedMatch)
+        {
+            var inputMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(inputBoard);
+            var intputGameState = GetDummyGameStateFromOccupiedTiles(inputMultiArray, lettersOnRack);
+
+            //set up our ITransformGameStateIntoTwoDimensionalArray object to return our multi-dimensional array
+            var mockArrayTransformer = new Mock<ITransformGameStateIntoTwoDimensionalArray>(MockBehavior.Strict);
+            mockArrayTransformer.Setup(mock => mock.Transform(intputGameState)).Returns(inputMultiArray);
+
+            var objectUnderTest = new DeterminePotentialPlayLocationsForNextWord(mockArrayTransformer.Object);
+
+            //get all the places to play, and then make sure they match the expected
+            var placesToPlay = objectUnderTest.GetPlacesToPlayNextWord(intputGameState).ToList();
+            placesToPlay.Count().Should().Be(1);
+            DoesPotentialPlayLocationMatch(GetDummyTwoDimensionalArrayFromOccupiedTilesString(expectedMatch), placesToPlay.First()).Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Verifies that the given input board and number of tiles results in the expected number of possible plays
+        /// </summary>
+        [TestCase(FiveByFiveJustTheMiddle, 1, 4)]
+        [TestCase(FiveByFiveJustTheMiddle, 2, 18)]
+        [TestCase(FiveByFiveJustTheMiddle, 3, 34)]
+        [TestCase(FiveByFiveJustTheMiddle, 4, 44)]
+        [TestCase(FiveByFiveJustTheMiddle, 5, 48)]
+        [TestCase(FiveByFiveJustTheMiddle, 6, 48)]
+        [TestCase(FiveByFiveWithBorderAndMiddle, 1, 8)]
+        [TestCase(FiveByFiveWithBorderAndMiddle, 2, 18)]
+        [TestCase(FiveByFiveWithBorderAndMiddle, 3, 22)]
+        [TestCase(FiveByFiveWithBorderAndMiddle, 4, 22)]
+        [TestCase(FifteenByFifteenWithFiveLetterWordToStart, 1, 12)]
+        [TestCase(FifteenByFifteenWithFiveLetterWordToStart, 2, 46)]
+        [TestCase(FifteenByFifteenWithFiveLetterWordToStart, 3, 90)]       
+        public void TestGetPlacesToPlayNextWordWithExpectedNumberOfMatches(string inputBoard, int lettersOnRack, int expectedMatchCount)
+        {
+            var inputMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(inputBoard);
+            var intputGameState = GetDummyGameStateFromOccupiedTiles(inputMultiArray, lettersOnRack);
+
+            //set up our ITransformGameStateIntoTwoDimensionalArray object to return our multi-dimensional array
+            var mockArrayTransformer = new Mock<ITransformGameStateIntoTwoDimensionalArray>(MockBehavior.Strict);
+            mockArrayTransformer.Setup(mock => mock.Transform(intputGameState)).Returns(inputMultiArray);
+
+            var objectUnderTest = new DeterminePotentialPlayLocationsForNextWord(mockArrayTransformer.Object);
+
+            //get all the places to play, and then make sure the count matches the expected
+            var placesToPlay = objectUnderTest.GetPlacesToPlayNextWord(intputGameState).ToList();
+            placesToPlay.Count().Should().Be(expectedMatchCount);
+        }
+
+        /// <summary>
+        /// Helper method that decides whether a given PotentialPlayLocation object "matches" a multi-dimensional array
+        /// where each cell is an occupied or not-occupied flag
+        /// </summary>
+        private bool DoesPotentialPlayLocationMatch(bool[,] onOffArray, PotentialPlayLocation potentialPlay)
+        {
+            //make sure that every cell that's activated matches a tile in the PotentialPlayLocation object
+            int totalLetters = 0;
+            for (int x = 0; x < onOffArray.GetLength(dimension: 0); x++)
+            {
+                for (int y = 0; y < onOffArray.GetLength(dimension: 1); y++)
+                {
+                    if (onOffArray[x, y])
+                    {
+                        if (potentialPlay.TileLocations.Any(tileLocation => tileLocation.X == x && tileLocation.Y == y) == false)
+                        {
+                            return false;
+                        }
+                        totalLetters++;
+                    }
+                }
+            }
+
+            //we know that every true flag in the onOffArray has a counter-part in the potentialPlay, now just check the counts on both are the same
+            return totalLetters == potentialPlay.TileLocations.Count();
+        }
+
+        /// <summary>
+        /// Helper method that takes a string that represents tiles already played on a board and converts into a multi-dimensional
+        /// array where each cell is a boolean flag that means "occupied" or not.  Anything but a space counts as occupied.
+        /// </summary>
+        private bool[,] GetDummyTwoDimensionalArrayFromOccupiedTilesString(string occupiedTiles)
+        {
+            var lines = occupiedTiles.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var multiDimensionalArray = new bool[lines.Length, lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    multiDimensionalArray[i, j] = (lines[i][j] != ' ');
+                }
+            }
+            return multiDimensionalArray;
+        }
+
+        /// <summary>
+        /// Helper method to generate a dummy GameState object based on occupied tiles on the board and a certain number of tiles in a rack.
+        /// In this case, the actual played tiles and the actual tiles in the rack don't matter...just that they exist.
+        /// </summary>
+        private GameState GetDummyGameStateFromOccupiedTiles(bool[,] occupiedTiles, int numberOfTilesInRack)
+        {
+            //put a certain number of X's on the rack
+            var rack = new char[numberOfTilesInRack];
+            for (int i = 0; i < numberOfTilesInRack; i++)
+            {
+                rack[i] = 'X';
+            }
+
+            var boardConfig = new BoardConfiguration
+            {
+                Height = occupiedTiles.GetLength(dimension: 0),
+                Width = occupiedTiles.GetLength(dimension: 1)
+            };
+
+            var challenge = new Challenge
+            {
+                BoardConfiguration = boardConfig,
+                MaxRackLength = numberOfTilesInRack
+            };
+
+            //make one gigantic word out of all the occupied tiles...even if it doesn't make sense...this implementation shouldn't care
+            var bigWord = new List<PlayedTile>();
+            for (int x = 0; x < boardConfig.Width; x++)
+            {
+                for (int y = 0; y < boardConfig.Height; y++)
+                {
+                    if (occupiedTiles[x, y])
+                    {
+                        bigWord.Add(new PlayedTile
+                        {
+                            Letter = 'A', // doesn't really matter what letter
+                            Location = new TileLocation {X = x, Y = y},
+                            WasBlank = false
+                        });
+                    }
+                }
+            }
+
+            return new GameState
+            {
+                Challenge = challenge,
+                PlayedWords =
+                    new List<PlayedWord>
+                    {
+                        new PlayedWord
+                        {
+                            TilesPlayed = bigWord
+                        }
+                    },
+                CurrentRack = new Rack { Tiles = rack }
+            };
+        }
+    }
+}
---
+++ src/WordsWithBrutes/Components/Impl/DeterminePotentialPlayLocationsForNextWord.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WordsWithBrutes.Enum;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components.Impl
+{
+    internal class DeterminePotentialPlayLocationsForNextWord : IDeterminePotentialPlayLocationsForNextWord
+    {
+        private readonly ITransformGameStateIntoTwoDimensionalArray _gameStateIntoTwoDimensionalArrayTransformer;
+
+        public DeterminePotentialPlayLocationsForNextWord(
+            ITransformGameStateIntoTwoDimensionalArray gameStateIntoTwoDimensionalArrayTransformer)
+        {
+            if (gameStateIntoTwoDimensionalArrayTransformer == null) throw new ArgumentNullException("gameStateIntoTwoDimensionalArrayTransformer");
+            _gameStateIntoTwoDimensionalArrayTransformer = gameStateIntoTwoDimensionalArrayTransformer;
+        }
+
+        /// <summary>
+        /// Analyzes the game state and returns a collection of places where one could play their next word
+        /// </summary>
+        public IEnumerable<PotentialPlayLocation> GetPlacesToPlayNextWord(GameState gameState)
+        {
+            //you can't play a word if there are no words to play on
+            if (gameState.PlayedWords.Any() == false) return Enumerable.Empty<PotentialPlayLocation>();
+
+            //figure out how many letters we can use on this play
+            int maxLettersThatCanBeUsed = Math.Min(gameState.Challenge.MaxRackLength, gameState.CurrentRack.Tiles.Count);
+
+            //convert the game state into a 2-d array and let another method take over
+            return GetPlacesToPlayNextWord(_gameStateIntoTwoDimensionalArrayTransformer.Transform(gameState), maxLettersThatCanBeUsed);
+        }
+
+        /// <summary>
+        /// Analyzes a 2-d matrix of occupiedTiles and, given the max number of letters that can be played at the current time,
+        /// returns a collection of places where one could play their next word
+        /// </summary>
+        private static IEnumerable<PotentialPlayLocation> GetPlacesToPlayNextWord(bool[,] occupiedTiles, int maxLettersThatCanBeUsed)
+        {
+            //iterate through every single unoccupied place on the board to play and figure out all the words that can be played using that place
+            //as the "top left" starting point of the word
+            var finalResults = new List<PotentialPlayLocation>();
+            for (int x = 0; x < occupiedTiles.GetLength(dimension: 0); x++)
+            {
+                for (int y = 0; y < occupiedTiles.GetLength(dimension: 1); y++)
+                {
+                    if (occupiedTiles[x, y] == false)
+                    {
+                        finalResults.AddRange(GetPlacesToPlayNextWord(occupiedTiles, maxLettersThatCanBeUsed, new TileLocation { X = x, Y = y }));
+                    }
+                }
+            }
+            return finalResults;
+        }
+
+        /// <summary>
+        /// Analyzes a 2-d matrix of occupiedTiles and, given the max number of letters that can be played at the current time and the "top left"
+        /// starting point for a word, returns a collection of places where one could play their next word
+        /// </summary>
+        private static IEnumerable<PotentialPlayLocation> GetPlacesToPlayNextWord(bool[,] occupiedTiles, int maxLettersThatCanBeUsed, TileLocation topLeftTileLocation)
+        {
+            var allPlays = new List<PotentialPlayLocation>();
+
+            //iterate through all the number of tiles that can be played in this turn and try each one
+            for (int i = 1; i <= maxLettersThatCanBeUsed; i++)
+            {
+                var possiblePlayHorizontally = GetPlacesToPlayNextWord(occupiedTiles, topLeftTileLocation, i, WordDirection.Horizontal);
+                if (possiblePlayHorizontally != null)
+                {
+                    allPlays.Add(possiblePlayHorizontally);
+                }
+
+                //checking horizontally and vertically for words that only use one tile is redundant, so don't do it
+                if (i == 1) continue;
+
+                var possiblePlayVertically = GetPlacesToPlayNextWord(occupiedTiles, topLeftTileLocation, i, WordDirection.Vertical);
+                if (possiblePlayVertically != null)
+                {
+                    allPlays.Add(possiblePlayVertically);
+                }
+            }
+
+            return allPlays;
+        }
+
+        /// <summary>
+        /// Analyzes a 2-d matrix of occupiedTiles and, given the EXACT number of letters that can be played at the current time and the "top left"
+        /// starting point for a word, returns either a PotentialPlayLocation if a word can be played OR NULL if a word cannot be played in this location
+        /// </summary>
+        private static PotentialPlayLocation GetPlacesToPlayNextWord(bool[,] occupiedTiles, TileLocation topLeftTileLocation, int numberOfTilesToUse,
+            WordDirection wordDirection)
+        {
+            var numColumns = occupiedTiles.GetLength(dimension: 0);
+            var numRows = occupiedTiles.GetLength(dimension: 1);
+
+            var play = new List<TileLocation> { topLeftTileLocation };
+
+            //keep track of the last place we played a tile since it's possible to "jump" or "skip" occupied tiles
+            var currentTileLocation = topLeftTileLocation;
+
+            int tilesPlayed = 1;
+            bool skippedOverAnExistingTile = false;
+            while (tilesPlayed < numberOfTilesToUse)
+            {
+                //make sure playing this word wouldn't take us off the board
+                var outOfRoom = (wordDirection == WordDirection.Horizontal && currentTileLocation.X >= numColumns - 1) ||
+                                (wordDirection == WordDirection.Vertical && currentTileLocation.Y >= numRows - 1);
+                if (outOfRoom) return null;
+
+                currentTileLocation = new TileLocation
+                {
+                    X = wordDirection == WordDirection.Horizontal ? currentTileLocation.X + 1 : currentTileLocation.X,
+                    Y = wordDirection == WordDirection.Vertical ? currentTileLocation.Y + 1 : currentTileLocation.Y,
+                };
+
+                //if there's a tile already there, move on, else we can add it to our possible play
+                if (occupiedTiles[currentTileLocation.X, currentTileLocation.Y] == false)
+                {
+                    play.Add(currentTileLocation);
+                    tilesPlayed++;
+                }
+                else
+                {
+                    //remember the fact that we "jumped" or "skipped" a tile because it guarantees this word is adjacent to some other word
+                    skippedOverAnExistingTile = true;
+                }
+            }
+
+            //if we had to skip over a tile, we know we're good to go
+            if (skippedOverAnExistingTile)
+            {
+                return new PotentialPlayLocation { TileLocations = play };
+            }
+
+            //if we get here, we've got room for the word, but we need to make sure its actually adjacent to a played tile
+            if (IsAdjacentToAPlayedTile(occupiedTiles, play))
+            {
+                return new PotentialPlayLocation { TileLocations = play };
+            }
+
+            //no word can be played of the specified length that starts at the specified place and goes in the specified direction
+            return null;
+        }
+
+        /// <summary>
+        /// Given a set of occupied tiles and a proposed location for a word, this method make sure it's a "legal" play where
+        /// at least one of the letters of the new word is adjacent to an occupied tile
+        /// </summary>
+        private static bool IsAdjacentToAPlayedTile(bool[,] occupiedTiles, IEnumerable<TileLocation> proposedLocationForWord)
+        {
+            var numColumns = occupiedTiles.GetLength(dimension: 0);
+            var numRows = occupiedTiles.GetLength(dimension: 1);
+
+            //iterate through all the tiles in the proposed location of the new word and look for an occupied tile to the left, right, above, or below it
+            foreach (var tileLocation in proposedLocationForWord)
+            {
+                //check to the left if we're not already all the way to the left
+                if (tileLocation.X > 0 && occupiedTiles[tileLocation.X - 1, tileLocation.Y]) return true;
+
+                //check to the right if we're not already all the way to the right
+                if (tileLocation.X < numColumns - 1 && occupiedTiles[tileLocation.X + 1, tileLocation.Y]) return true;
+
+                //check above if we're not already all the way to the top
+                if (tileLocation.Y > 0 && occupiedTiles[tileLocation.X, tileLocation.Y - 1]) return true;
+
+                //check to the below  if we're not already all the way to the bottom
+                if (tileLocation.Y < numRows - 1 && occupiedTiles[tileLocation.X, tileLocation.Y + 1]) return true;
+            }
+
+            //if we get here, we didn't find anything adjacent
+            return false;
+        }
+    }
+}
