e4dcc6b58a060eb9c982280050a2975ce1c4153a
Andrew Lane <andrew.lane@hobsons.com>
2014-03-16 20:02:31 -0400

    Adding a IGenerateWordsToTryForPotentialPlayLocation component that will be responsible for generating potential words to play based on a given potential play location

---
+++ src/WordsWithBrutes/Components/IGenerateWordsToTryForPotentialPlayLocation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components
+{
+    /// <summary>
+    /// Responsible for generating all the different permutations of possible words to try given a PotentialPlayLocation object
+    /// </summary>
+    public interface IGenerateWordsToTryForPotentialPlayLocation
+    {
+        IEnumerable<PlayedWord> Generate(PotentialPlayLocation playLocation);
+    }
+}
