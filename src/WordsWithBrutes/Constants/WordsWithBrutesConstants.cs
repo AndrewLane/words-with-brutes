7069e552769f4993f5236c8bd34b5fbf030c7186
Andrew Lane <andrew.lane@hobsons.com>
2014-03-18 15:10:42 -0400

    Adding a constants file

---
+++ src/WordsWithBrutes/Constants/WordsWithBrutesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace WordsWithBrutes.Constants
+{
+    public static class WordsWithBrutesConstants
+    {
+        /// <summary>
+        /// The representation of a "blank" tile
+        /// </summary>
+        public const char BlankTile = '_';
+    }
+}
