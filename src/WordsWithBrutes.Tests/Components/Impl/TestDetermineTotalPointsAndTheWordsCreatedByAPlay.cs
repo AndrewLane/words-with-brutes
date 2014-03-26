a3cc708cdd7645c2ea0df50a857583c47884f9a2
Andrew Lane <andrew.lane@hobsons.com>
2014-03-25 23:40:32 -0400

    Adding the algorithm to track the scoring to the DetermineTheTotalPointsAndWordsCreatedByAPlay implementation Also updated the unit tests This class got pretty unwieldy and is probably responsible for doing way too much and has too much repetition, so it's a TODO to refactor it later

--- src/WordsWithBrutes.Tests/Components/Impl/TestDetermineTotalPointsAndTheWordsCreatedByAPlay.cs
+++ src/WordsWithBrutes.Tests/Components/Impl/TestDetermineTotalPointsAndTheWordsCreatedByAPlay.cs
@@ -6,6 +6,8 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using WordsWithBrutes.Components;
 using WordsWithBrutes.Components.Impl;
+using WordsWithBrutes.Constants;
+using WordsWithBrutes.Enum;
 using WordsWithBrutes.Model;
 
 namespace WordsWithBrutes.Tests.Components.Impl
@@ -16,19 +18,63 @@ namespace WordsWithBrutes.Tests.Components.Impl
     [TestFixture]
     public class TestDetermineTotalPointsAndTheWordsCreatedByAPlay
     {
-        private static readonly GameState DummyGameState = new GameState();
+        private static readonly TilePointConfiguration DummyTilePointConfiguration = new TilePointConfiguration
+        {
+            PointValues = new Dictionary<char, int>
+            {
+                {WordsWithBrutesConstants.BlankTile, 0},
+                {'A', 1},
+                {'B', 2},
+                {'C', 3},
+                {'D', 4},
+                {'E', 1},
+                {'F', 2},
+                {'G', 3},
+                {'H', 4},
+                {'I', 1},
+                {'J', 2},
+                {'K', 3},
+                {'L', 4},
+                {'M', 1},
+                {'N', 2},
+                {'O', 3},
+                {'P', 4},
+                {'Q', 1},
+                {'R', 2},
+                {'S', 3},
+                {'T', 4},
+                {'U', 1},
+                {'V', 2},
+                {'W', 3},
+                {'X', 4},
+                {'Y', 1},
+                {'Z', 2}
+            }
+        };
+
+        private static readonly GameState DummyGameState = new GameState
+        {
+            Challenge = new Challenge
+            {
+                MaxRackLength = 7,
+                MaxRackLengthBonus = 35,
+                TilePointConfiguration = DummyTilePointConfiguration,
+                BoardConfiguration = new BoardConfiguration()
+            }
+        };
 
         #region constants for easy-to-write tests
-        const string TwoByTwoAllAsUpperLeftFree = @"
+
+        private const string TwoByTwoAllAsUpperLeftFree = @"
  A
 AA";
-        const string TwoByTwoAllAsUpperRightFree = @"
+        private const string TwoByTwoAllAsUpperRightFree = @"
 A 
 AA";
-        const string TwoByTwoAllAsLowerLeftFree = @"
+        private const string TwoByTwoAllAsLowerLeftFree = @"
 AA
  A";
-        const string TwoByTwoAllAsLowerRightFree = @"
+        private const string TwoByTwoAllAsLowerRightFree = @"
 AA
 A ";
 
@@ -37,16 +83,16 @@ ABC
 D F
 GHI";
 
-        const string TwoByTwoAInUpperLeft = @"
+        private const string TwoByTwoAInUpperLeft = @"
 A 
   ";
-        const string TwoByTwoAInUpperRight = @"
+        private const string TwoByTwoAInUpperRight = @"
  A
   ";
-        const string TwoByTwoAInLowerLeft = @"
+        private const string TwoByTwoAInLowerLeft = @"
   
 A ";
-        const string TwoByTwoAInLowerRight = @"
+        private const string TwoByTwoAInLowerRight = @"
   
  A";
         private const string XinAThreeByThreeHole = @"
@@ -70,6 +116,13 @@ ABC";
 ABC
    
    ";
+        private const string TwoByTwoBoardAaTopRow = @"
+AA
+  ";
+
+        private const string EmptyTwoByTwoBoard = @"
+  
+  ";
         private const string EmptySevenBySevenBoard = @"
        
        
@@ -84,7 +137,7 @@ ABC
        
        
        
- TEST         
+ TeST         
        
        ";
         private const string WideBoardAbcdAtTheUpperLeft = @"
@@ -122,26 +175,29 @@ ABCD
         #endregion constants for easy-to-write tests
 
         /// <summary>
-        /// Tests the GetPlayedWords method
+        /// Tests the TestGetPlayedWordsAndTotalPoints method
         /// </summary>
-        [TestCase(TwoByTwoAllAsUpperLeftFree, TwoByTwoAInUpperLeft, "AA")]
-        [TestCase(TwoByTwoAllAsUpperRightFree, TwoByTwoAInUpperRight, "AA")]
-        [TestCase(TwoByTwoAllAsLowerLeftFree, TwoByTwoAInLowerLeft, "AA")]
-        [TestCase(TwoByTwoAllAsLowerRightFree, TwoByTwoAInLowerRight, "AA")]
-        [TestCase(ThreeByThreeWithHole, XinAThreeByThreeHole, "BXH,DXF")]
-        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcLeftColumn, "ABC,BX")]
-        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcRightColumn, "ABC,XB")]
-        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcTopRow, "ABC,BX")]
-        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcBottomRow, "ABC,XB")]
-        [TestCase(EmptySevenBySevenBoard, SevenBySevenBoardWithTestinTheMiddle, "TEST")]
-        [TestCase(WideBoardAbcdAtTheUpperLeft, WideBoardGhijAtTheLowerRight, "GHIJ,BG,CH,DI")]
-        [TestCase(WideBoardGhijAtTheLowerRight, WideBoardAbcdAtTheUpperLeft, "ABCD,BG,CH,DI")]
-        [TestCase(SevenBySevenLeapFrogSetup, SevenBySevenLeapFrogWordOnTheLeft, "RDSJTPU,SF,TL")]
-        [TestCase(SevenBySevenLeapFrogSetup, SevenBySevenLeapFrogWordOnTheRight, "RSTUVWX,ABCDES,FT,GHIJKU,LV,MNOPQW")]
-        public void TestGetPlayedWords(string inputBoard, string playedTiles, string expectedWordsPlayed)
+        [TestCase(TwoByTwoAllAsUpperLeftFree, TwoByTwoAInUpperLeft, "AA", 12)]
+        [TestCase(TwoByTwoAllAsUpperRightFree, TwoByTwoAInUpperRight, "AA", 8)]
+        [TestCase(TwoByTwoAllAsLowerLeftFree, TwoByTwoAInLowerLeft, "AA", 6)]
+        [TestCase(TwoByTwoAllAsLowerRightFree, TwoByTwoAInLowerRight, "AA", 4)]
+        [TestCase(ThreeByThreeWithHole, XinAThreeByThreeHole, "BXH,DXF", 20)]
+        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcLeftColumn, "ABC,BX", 32)]
+        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcRightColumn, "ABC,XB", 24)]
+        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcTopRow, "ABC,BX", 40)]
+        [TestCase(XinAThreeByThreeHole, ThreeByThreeAbcBottomRow, "ABC,XB", 30)]
+        [TestCase(EmptySevenBySevenBoard, SevenBySevenBoardWithTestinTheMiddle, "TEST", 50)]
+        [TestCase(WideBoardAbcdAtTheUpperLeft, WideBoardGhijAtTheLowerRight, "GHIJ,BG,CH,DI", 37)]
+        [TestCase(WideBoardGhijAtTheLowerRight, WideBoardAbcdAtTheUpperLeft, "ABCD,BG,CH,DI", 95)]
+        [TestCase(SevenBySevenLeapFrogSetup, SevenBySevenLeapFrogWordOnTheLeft, "RDSJTPU,SF,TL", 61)]
+        [TestCase(SevenBySevenLeapFrogSetup, SevenBySevenLeapFrogWordOnTheRight, "RSTUVWX,ABCDES,FT,GHIJKU,LV,MNOPQW", 174)]
+        public void TestGetPlayedWordsAndTotalPoints(string inputBoard, string playedTiles, string expectedWordsPlayed, int expectedTotalPoints)
         {
             //create the array that the ITransformGameStateIntoTwoDimensionalArray mock will return
             var inputMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(inputBoard);
+
+            //assign the special tiles to the board configuration
+            AssignSpecialTiles(inputMultiArray.GetLength(dimension: 0), inputMultiArray.GetLength(dimension: 1), DummyGameState.Challenge.BoardConfiguration);
 
             //set up our ITransformGameStateIntoTwoDimensionalArray object to return our multi-dimensional array
             var mockArrayTransformer = new Mock<ITransformGameStateIntoTwoDimensionalArray>(MockBehavior.Strict);
@@ -152,9 +208,72 @@ ABCD
             //create our PlayedTile collection
             var playedTileList = GetPlayedTileListFromOccupiedTilesString(playedTiles);
 
-            //run the test and make sure the returned words match the expected words exactly
+            //run the test and make sure the returned words match the expected words exactly as well as the total points awarded
             var playedWordsAndTotalPoints = objectUnderTest.GetPlayedWordsAndTotalPoints(DummyGameState, playedTileList);
-            playedWordsAndTotalPoints.WordsPlayed.ShouldAllBeEquivalentTo(expectedWordsPlayed.Split(new[] { ',' }));
+            playedWordsAndTotalPoints.WordsPlayed.ShouldAllBeEquivalentTo(expectedWordsPlayed.Split(new[] {','}));
+            playedWordsAndTotalPoints.TotalPointsAwarded.Should().Be(expectedTotalPoints);
+        }
+
+        /// <summary>
+        /// Make sure that hitting two double word tiles ends up quadrupling your score
+        /// </summary>
+        [Test]
+        public void TestGetPlayedWordsAndTotalPointsForDoubleDoubleWord()
+        {
+            //create the array that the ITransformGameStateIntoTwoDimensionalArray mock will return
+            var inputMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(EmptyTwoByTwoBoard);
+
+            //assign the two double word tiles on the top row
+            DummyGameState.Challenge.BoardConfiguration.SpecialTiles = new List<SpecialTile>
+            {
+                new SpecialTile {Location = new TileLocation {X = 0, Y=0},TileType = SpecialTileType.DoubleWord},
+                new SpecialTile {Location = new TileLocation {X = 1, Y=0},TileType = SpecialTileType.DoubleWord}
+            };
+
+            //set up our ITransformGameStateIntoTwoDimensionalArray object to return our multi-dimensional array
+            var mockArrayTransformer = new Mock<ITransformGameStateIntoTwoDimensionalArray>(MockBehavior.Strict);
+            mockArrayTransformer.Setup(mock => mock.TransformIntoPlayedTileMultiArray(DummyGameState)).Returns(inputMultiArray);
+
+            var objectUnderTest = new DetermineTheTotalPointsAndWordsCreatedByAPlay(mockArrayTransformer.Object);
+
+            //create our PlayedTile collection
+            var playedTileList = GetPlayedTileListFromOccupiedTilesString(TwoByTwoBoardAaTopRow);
+
+            //run the test and make sure the returned words match the expected words exactly as well as the total points awarded
+            var playedWordsAndTotalPoints = objectUnderTest.GetPlayedWordsAndTotalPoints(DummyGameState, playedTileList);
+            playedWordsAndTotalPoints.WordsPlayed.ShouldAllBeEquivalentTo("AA".Split(new[] { ',' }));
+            playedWordsAndTotalPoints.TotalPointsAwarded.Should().Be(8);
+        }
+
+        /// <summary>
+        /// Make sure that hitting two triple word tiles ends up multiplying your score by 9
+        /// </summary>
+        [Test]
+        public void TestGetPlayedWordsAndTotalPointsForTripleTripleWord()
+        {
+            //create the array that the ITransformGameStateIntoTwoDimensionalArray mock will return
+            var inputMultiArray = GetDummyTwoDimensionalArrayFromOccupiedTilesString(EmptyTwoByTwoBoard);
+
+            //assign the two triple word tiles on the top row
+            DummyGameState.Challenge.BoardConfiguration.SpecialTiles = new List<SpecialTile>
+            {
+                new SpecialTile {Location = new TileLocation {X = 0, Y=0},TileType = SpecialTileType.TripleWord},
+                new SpecialTile {Location = new TileLocation {X = 1, Y=0},TileType = SpecialTileType.TripleWord}
+            };
+
+            //set up our ITransformGameStateIntoTwoDimensionalArray object to return our multi-dimensional array
+            var mockArrayTransformer = new Mock<ITransformGameStateIntoTwoDimensionalArray>(MockBehavior.Strict);
+            mockArrayTransformer.Setup(mock => mock.TransformIntoPlayedTileMultiArray(DummyGameState)).Returns(inputMultiArray);
+
+            var objectUnderTest = new DetermineTheTotalPointsAndWordsCreatedByAPlay(mockArrayTransformer.Object);
+
+            //create our PlayedTile collection
+            var playedTileList = GetPlayedTileListFromOccupiedTilesString(TwoByTwoBoardAaTopRow);
+
+            //run the test and make sure the returned words match the expected words exactly as well as the total points awarded
+            var playedWordsAndTotalPoints = objectUnderTest.GetPlayedWordsAndTotalPoints(DummyGameState, playedTileList);
+            playedWordsAndTotalPoints.WordsPlayed.ShouldAllBeEquivalentTo("AA".Split(new[] { ',' }));
+            playedWordsAndTotalPoints.TotalPointsAwarded.Should().Be(18);
         }
 
         /// <summary>
@@ -163,7 +282,7 @@ ABCD
         /// </summary>
         private static PlayedTile[,] GetDummyTwoDimensionalArrayFromOccupiedTilesString(string occupiedTiles)
         {
-            var lines = occupiedTiles.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var lines = occupiedTiles.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
             var multiDimensionalArray = new PlayedTile[lines.First().Length, lines.Length];
             for (int y = 0; y < lines.Length; y++)
             {
@@ -171,7 +290,7 @@ ABCD
                 {
                     multiDimensionalArray[x, y] = (lines[y][x] == ' ')
                         ? null
-                        : new PlayedTile {Letter = lines[y][x], Location = new TileLocation {X = x, Y = y}, WasBlank = false};
+                        : new PlayedTile {Letter = char.ToUpper(lines[y][x]), Location = new TileLocation {X = x, Y = y}, WasBlank = char.IsLower(lines[y][x])};
                 }
             }
             return multiDimensionalArray;
@@ -182,7 +301,7 @@ ABCD
         /// </summary>
         private static IEnumerable<PlayedTile> GetPlayedTileListFromOccupiedTilesString(string occupiedTiles)
         {
-            var lines = occupiedTiles.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var lines = occupiedTiles.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
             var playedTiles = new List<PlayedTile>();
             for (int y = 0; y < lines.Length; y++)
             {
@@ -190,11 +309,77 @@ ABCD
                 {
                     if (lines[y][x] != ' ')
                     {
-                        playedTiles.Add(new PlayedTile { Letter = lines[y][x], Location = new TileLocation { X = x, Y = y } });
+                        playedTiles.Add(new PlayedTile
+                        {
+                            Letter = char.ToUpper(lines[y][x]),
+                            Location = new TileLocation {X = x, Y = y},
+                            WasBlank = char.IsLower(lines[y][x])
+                        });
                     }
                 }
             }
             return playedTiles;
+        }
+
+        /// <summary>
+        /// Helper method that assigns special tiles as follows where T represents triple word, D represents double word, 2 represents double letter,
+        /// and 3 represents triple letter
+        /// 
+        /// T3_3_3_3_3_3_3_
+        /// 2_2_2_2_2_2_2_2
+        /// _3D3_3_3_3_3_3_
+        /// 2_2T2_2_2_2_2_2
+        /// _3_3D3_3_3_3_3_
+        /// 2_2_2_2_2_2_2_2
+        /// _3_3_3T3_3_3_3_
+        /// 2_2_2_2_2_2_2_2
+        /// _3_3_3_3D3_3_3_
+        /// 2_2_2_2_2T2_2_2
+        /// _3_3_3_3_3D3_3_
+        /// 2_2_2_2_2_2_2_2
+        /// _3_3_3_3_3_3T3_
+        /// 2_2_2_2_2_2_2_2
+        /// _3_3_3_3_3_3_3D
+        /// </summary>
+        private static void AssignSpecialTiles(int widthOfBoard, int heightOfBoard, BoardConfiguration boardConfiguration)
+        {
+            var specialTiles = new List<SpecialTile>();
+
+            for (int x = 0; x < widthOfBoard; x++)
+            {
+                for (int y = 0; y < heightOfBoard; y++)
+                {
+                    var specialTile = new SpecialTile {Location = new TileLocation {X = x, Y = y}};
+
+                    //create a triple word if both the X and Y are divisible by 3 and they are equal
+                    if (x%3 == 0 && x == y)
+                    {
+                        specialTile.TileType = SpecialTileType.TripleWord;
+                        specialTiles.Add(specialTile);
+                    }
+                    //create a double word if both the X and Y are divisible by 2 and they are equal
+                    else if (x%2 == 0 && x == y)
+                    {
+                        specialTile.TileType = SpecialTileType.DoubleWord;
+                        specialTiles.Add(specialTile);
+                    } 
+                    //create a double letter if X is even and Y is odd
+                    else if (x%2 == 0 && y%2 == 1)
+                    {
+                        specialTile.TileType = SpecialTileType.DoubleLetter;
+                        specialTiles.Add(specialTile);
+
+                    }
+                    //create a triple letter if X is odd and Y is even
+                    else if (x%2 == 1 && y%2 == 0)
+                    {
+                        specialTile.TileType = SpecialTileType.TripleLetter;
+                        specialTiles.Add(specialTile);
+                    }
+                }
+            }
+
+            boardConfiguration.SpecialTiles = specialTiles;
         }
     }
 }
--- src/WordsWithBrutes/Components/Impl/DetermineTheTotalPointsAndWordsCreatedByAPlay.cs
+++ src/WordsWithBrutes/Components/Impl/DetermineTheTotalPointsAndWordsCreatedByAPlay.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using WordsWithBrutes.Constants;
+using WordsWithBrutes.Enum;
 using WordsWithBrutes.Model;
 
 namespace WordsWithBrutes.Components.Impl
@@ -23,7 +25,7 @@ namespace WordsWithBrutes.Components.Impl
         public WordsPlayedAndPointsScored GetPlayedWordsAndTotalPoints(GameState gameState, IEnumerable<PlayedTile> playedTiles)
         {
             //harden the list since we'll enumerate it many times
-            playedTiles = playedTiles.ToList();
+            var playedTilesList = playedTiles.ToList();
 
             var playedWords = new List<string>();
 
@@ -31,30 +33,75 @@ namespace WordsWithBrutes.Components.Impl
             var existingWords = _gameStateTransformer.TransformIntoPlayedTileMultiArray(gameState);
 
             //add all the played tile to the multi-dimensional array so we have the state of the board after the word is played
-            foreach (var playedTile in playedTiles)
+            foreach (var playedTile in playedTilesList)
             {
                 existingWords[playedTile.Location.X, playedTile.Location.Y] = playedTile;
             }
 
+            var specialTiles = gameState.Challenge.BoardConfiguration.SpecialTiles.ToList();
+            var tilePointConfiguration = gameState.Challenge.TilePointConfiguration;
+
+            //figure out which direction the word was played
+            var directionOfPlayedWord = playedTilesList.Select(playedTile => playedTile.Location.Y).Distinct().Count() > 1
+                ? WordDirection.Vertical
+                : WordDirection.Horizontal;
+
+            int totalPoints = 0;
+            int pointsForTheWord = 0;
             //loop through all the played tiles and look for horizontal and vertical words
-            playedWords.AddRange(
-                playedTiles.Select(playedTile => GetHorizontalWord(existingWords, playedTile.Location))
-                    .Where(possibleNewWord => possibleNewWord.IsNullOrEmpty() == false));
-            playedWords.AddRange(
-                playedTiles.Select(playedTile => GetVerticalWord(existingWords, playedTile.Location))
-                    .Where(possibleNewWord => possibleNewWord.IsNullOrEmpty() == false));
+            foreach (
+                var newHorizontalWord in
+                    playedTilesList.Select(
+                        playedTile => GetHorizontalWord(specialTiles, playedTilesList, tilePointConfiguration, existingWords, playedTile.Location,
+                            out pointsForTheWord)).Where(newHorizontalWord => newHorizontalWord.IsNullOrEmpty() == false))
+            {
+                playedWords.Add(newHorizontalWord);
+                totalPoints += pointsForTheWord;
+
+                //if we played the word in the horizontal direction, we're done because we don't want to count it many times
+                if (directionOfPlayedWord == WordDirection.Horizontal)
+                {
+                    break;
+                }
+            }
+            foreach (
+                var newVerticalWord in
+                    playedTilesList.Select(
+                        playedTile => GetVerticalWord(specialTiles, playedTilesList, tilePointConfiguration, existingWords, playedTile.Location,
+                            out pointsForTheWord)).Where(newVerticalWord => newVerticalWord.IsNullOrEmpty() == false))
+            {
+                playedWords.Add(newVerticalWord);
+                totalPoints += pointsForTheWord;
+
+                //if we played the word in the vertical direction, we're done because we don't want to count it many times
+                if (directionOfPlayedWord == WordDirection.Vertical)
+                {
+                    break;
+                }
+            }
+
+            //look for the bonus for playing the maximum number of tiles at once
+            if (playedTilesList.Count() == gameState.Challenge.MaxRackLength)
+            {
+                totalPoints += gameState.Challenge.MaxRackLengthBonus;
+            }
 
             return new WordsPlayedAndPointsScored
             {
-                WordsPlayed = playedWords.Distinct()
+                WordsPlayed = playedWords.Distinct(),
+                TotalPointsAwarded = totalPoints
             };
         }
 
         /// <summary>
         /// Helper which looks for words formed in the up-and-down direction
         /// </summary>
-        private static string GetVerticalWord(PlayedTile[,] existingWords, TileLocation tileLocationThatsPartOfTheWord)
+        private static string GetVerticalWord(IList<SpecialTile> specialTiles, IList<PlayedTile> tilesPlayedThisTurn,
+            TilePointConfiguration tilePointConfiguration, PlayedTile[,] existingWords,
+            TileLocation tileLocationThatsPartOfTheWord, out int pointsForTheWord)
         {
+            pointsForTheWord = 0;
+
             //find the top-most played tile and the bottom-most played tile
             var smallestYCoordinate = tileLocationThatsPartOfTheWord.Y;
             var largestYCoordinate = smallestYCoordinate;
@@ -91,20 +138,59 @@ namespace WordsWithBrutes.Components.Impl
                 return null;
             }
 
+            //keep track of any word multipliers
+            int pointMultiplier = 1;
+
             //make a word from top to bottom
             var newWord = new StringBuilder();
             for (int y = smallestYCoordinate; y <= largestYCoordinate; y++)
             {
-                newWord.Append(existingWords[tileLocationThatsPartOfTheWord.X, y].Letter);
+                var tileAtThisLocation = existingWords[tileLocationThatsPartOfTheWord.X, y];
+
+                int pointsForThisTile =
+                    tilePointConfiguration.PointValues[tileAtThisLocation.WasBlank ? WordsWithBrutesConstants.BlankTile : tileAtThisLocation.Letter];
+
+                bool thisTileWasPlayedWithThisTurn = tilesPlayedThisTurn.Any(tile => tile.Location.X == tileLocationThatsPartOfTheWord.X && tile.Location.Y == y);
+                if (thisTileWasPlayedWithThisTurn)
+                {
+                    var specialTileAtThisLocation =
+                        specialTiles.FirstOrDefault(specialTile => specialTile.Location.X == tileLocationThatsPartOfTheWord.X && specialTile.Location.Y == y);
+                    if (specialTileAtThisLocation != null)
+                    {
+                        switch (specialTileAtThisLocation.TileType)
+                        {
+                            case SpecialTileType.DoubleLetter:
+                                pointsForThisTile *= 2;
+                                break;
+                            case SpecialTileType.DoubleWord:
+                                pointMultiplier *= 2;
+                                break;
+                            case SpecialTileType.TripleLetter:
+                                pointsForThisTile *= 3;
+                                break;
+                            case SpecialTileType.TripleWord:
+                                pointMultiplier *= 3;
+                                break;
+                        }
+                    }
+                }
+                pointsForTheWord += pointsForThisTile;
+                newWord.Append(tileAtThisLocation.Letter);
             }
+
+            pointsForTheWord = pointMultiplier*pointsForTheWord;
             return newWord.ToString();
         }
 
         /// <summary>
         /// Helper which looks for words formed in the left-to-right direction
         /// </summary>
-        private static string GetHorizontalWord(PlayedTile[,] existingWords, TileLocation tileLocationThatsPartOfTheWord)
+        private static string GetHorizontalWord(IList<SpecialTile> specialTiles, IList<PlayedTile> tilesPlayedThisTurn,
+            TilePointConfiguration tilePointConfiguration, PlayedTile[,] existingWords,
+            TileLocation tileLocationThatsPartOfTheWord, out int pointsForTheWord)
         {
+            pointsForTheWord = 0;
+
             //find the left-most played tile and the right-most played tile
             var smallestXCoordinate = tileLocationThatsPartOfTheWord.X;
             var largestXCoordinate = smallestXCoordinate;
@@ -141,12 +227,47 @@ namespace WordsWithBrutes.Components.Impl
                 return null;
             }
 
+            //keep track of any word multipliers
+            int pointMultiplier = 1;
+
             //make a word from left to right
             var newWord = new StringBuilder();
             for (int x = smallestXCoordinate; x <= largestXCoordinate; x++)
             {
-                newWord.Append(existingWords[x, tileLocationThatsPartOfTheWord.Y].Letter);
+                var tileAtThisLocation = existingWords[x, tileLocationThatsPartOfTheWord.Y];
+
+                int pointsForThisTile =
+                    tilePointConfiguration.PointValues[tileAtThisLocation.WasBlank ? WordsWithBrutesConstants.BlankTile : tileAtThisLocation.Letter];
+
+                bool thisTileWasPlayedWithThisTurn = tilesPlayedThisTurn.Any(tile => tile.Location.X == x && tile.Location.Y == tileLocationThatsPartOfTheWord.Y);
+                if (thisTileWasPlayedWithThisTurn)
+                {
+                    var specialTileAtThisLocation =
+                        specialTiles.FirstOrDefault(specialTile => specialTile.Location.X == x && specialTile.Location.Y == tileLocationThatsPartOfTheWord.Y);
+                    if (specialTileAtThisLocation != null)
+                    {
+                        switch (specialTileAtThisLocation.TileType)
+                        {
+                            case SpecialTileType.DoubleLetter:
+                                pointsForThisTile *= 2;
+                                break;
+                            case SpecialTileType.DoubleWord:
+                                pointMultiplier *= 2;
+                                break;
+                            case SpecialTileType.TripleLetter:
+                                pointsForThisTile *= 3;
+                                break;
+                            case SpecialTileType.TripleWord:
+                                pointMultiplier *= 3;
+                                break;
+                        }
+                    }
+                }
+                pointsForTheWord += pointsForThisTile;
+                newWord.Append(tileAtThisLocation.Letter);
             }
+
+            pointsForTheWord = pointMultiplier*pointsForTheWord;
             return newWord.ToString();
         }
     }
