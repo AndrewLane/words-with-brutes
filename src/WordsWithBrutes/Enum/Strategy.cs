116872013d213ed019606dc86e6138919891d5ce
Andrew Lane <andrew.lane@hobsons.com>
2014-03-26 15:13:53 -0400

    Adding an enum of the different algorithms this tool will use to solve a given challenge

---
+++ src/WordsWithBrutes/Enum/Strategy.cs
@@ -0,0 +1,23 @@
+﻿namespace WordsWithBrutes.Enum
+{
+    /// <summary>
+    /// Models the different strategies for solving a given challenge
+    /// </summary>
+    public enum Strategy
+    {
+        /// <summary>
+        /// Attains the highest point total possible
+        /// </summary>
+        MaxTotalPoints,
+
+        /// <summary>
+        /// Maximizes each word played instead of maximizing the overall score
+        /// </summary>
+        MaxPointsEachWord,
+
+        /// <summary>
+        /// Just tries to get the challenge solved and doesn't care about points (i.e. pick the quickest possible solution)
+        /// </summary>
+        QuickSolve
+    }
+}
