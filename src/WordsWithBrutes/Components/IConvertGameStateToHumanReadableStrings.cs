0707fa4abc89423452ab9c7812735dff8072c664
Andrew Lane <andrew.lane@hobsons.com>
2014-03-30 09:42:22 -0400

    Add a new IConvertGameStateToHumanReadableStrings component that's responsible for taking a GameState and printing the steps to reach that GameState (i.e. all the played words) to the user

---
+++ src/WordsWithBrutes/Components/IConvertGameStateToHumanReadableStrings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components
+{
+    /// <summary>
+    /// Responsible for taking a GameState and converting into a collection of human-readable strings that can be presented to the user
+    /// as a step-by-step way to get to that particular GameState from the start
+    /// </summary>
+    public interface IConvertGameStateToHumanReadableStrings
+    {
+        /// <summary>
+        /// Convert the played words of this GameState into human-readable strings
+        /// </summary>
+        IEnumerable<string> Print(GameState gameState);
+    }
+}
