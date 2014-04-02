e176044bca5c7bace350434d28c014e79233c811
Andrew Lane <andrew.lane@hobsons.com>
2014-04-01 21:48:11 -0400

    Adding a new ISolveChallenges component

---
+++ src/WordsWithBrutes/Components/ISolveChallenges.cs
@@ -0,0 +1,16 @@
+﻿using WordsWithBrutes.Enum;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components
+{
+    /// <summary>
+    /// Responsible for solving a challenge using a given strategy
+    /// </summary>
+    public interface ISolveChallenges
+    {
+        /// <summary>
+        /// Solves the given challenge using the given strategy and returns the GameState of what it took to get there
+        /// </summary>
+        GameState Solve(Challenge challenge, Strategy strategy);
+    }
+}
