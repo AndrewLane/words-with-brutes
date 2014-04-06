c691bfe9db0b5bd6bd017f519f1a7f2e71e8c0c8
Andrew Lane <andrew.lane@hobsons.com>
2014-04-06 16:25:11 -0400

    Adding an IConvertStringToWordsWithFriendsChallenge component

---
+++ src/WordsWithBrutes/Components/IConvertStringToWordsWithFriendsChallenge.cs
@@ -0,0 +1,15 @@
+﻿using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components
+{
+    /// <summary>
+    /// Responsible for taking a string representing played tiles and converting it to a standard words with friends Challenge object
+    /// </summary>
+    public interface IConvertStringToWordsWithFriendsChallenge
+    {
+        /// <summary>
+        /// Take a string representing played tiles and convert it to a Challenge object with the standard WWF setup
+        /// </summary>
+        Challenge Convert(string tiles);
+    }
+}
---
+++ src/WordsWithBrutes/Components/Impl/ConvertStringToWordsWithFriendsChallenge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components.Impl
+{
+    internal class ConvertStringToWordsWithFriendsChallenge : IConvertStringToWordsWithFriendsChallenge
+    {
+        private readonly IPopulateStandardWordsWithFriendsChallenge _standardChallengeSeeder;
+
+        public ConvertStringToWordsWithFriendsChallenge(IPopulateStandardWordsWithFriendsChallenge standardChallengeSeeder)
+        {
+            if (standardChallengeSeeder == null) throw new ArgumentNullException("standardChallengeSeeder");
+            _standardChallengeSeeder = standardChallengeSeeder;
+        }
+
+        /// <summary>
+        /// Take a string representing played tiles and convert it to a Challenge object with the standard WWF setup
+        /// </summary>
+        public Challenge Convert(string tiles)
+        {
+            var baseChallenge = _standardChallengeSeeder.GetStandardChallenge();
+
+            var lines = tiles.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            var startingTiles = new List<PlayedTile>();
+
+            if (lines.Length != 15)
+            {
+                throw new ArgumentException(String.Format("tiles has {0} rows instead of 15", lines.Length));
+            }
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                if (lines[y].Length != 15)
+                {
+                    throw new ArgumentException(String.Format("line {0} has {1} columns instead of 15", (y + 1), lines[y].Length));
+                }
+
+                for (int x = 0; x < 15; x++)
+                {
+                    if (lines[y][x] == ' ') continue;
+
+                    startingTiles.Add(new PlayedTile
+                    {
+                        Letter = char.ToUpper(lines[y][x]),
+                        Location = new TileLocation { X = x, Y = y },
+                        WasBlank = char.IsLower(lines[y][x])
+                    });
+                }
+            }
+
+            baseChallenge.StartingTiles = startingTiles;
+            return baseChallenge;
+        }
+    }
+}
