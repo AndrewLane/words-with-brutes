a922ea23107411d8006078ff31374c4288913831
Andrew Lane <andrew.lane@hobsons.com>
2015-10-30 17:04:40 -0400

    Add a WindsorInstaller to register all our components, and then add some integration tests so we can start testing the functionality from end-to-end

---
+++ src/WordsWithBrutes.Tests/IntegrationTests/SolverTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Windsor;
+using FluentAssertions;
+using NUnit.Framework;
+using WordsWithBrutes.Components;
+using WordsWithBrutes.DependencyInjection;
+using WordsWithBrutes.Enum;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Tests.IntegrationTests
+{
+    /// <summary>
+    /// Integration tests that demonstrate accurate solutions
+    /// </summary>
+    [TestFixture]
+    public class SolverTests
+    {
+        /// <summary>
+        /// Verifies we can put ING on the end of TEST for TESTING.  This should be 20 points because of a DW.
+        /// </summary>
+        [Test]
+        public void SimpleTest()
+        {
+            var container = new WindsorContainer();
+            try
+            {
+                container.Install(new WindsorInstaller());
+
+                var challengeCreator = container.Resolve<IConvertStringToWordsWithFriendsChallenge>();
+                var solutionPrinter = container.Resolve<IConvertGameStateToHumanReadableStrings>();
+                var solver = container.Resolve<ISolveChallenges>();
+
+                const string board = @"
+               
+               
+               
+               
+               
+               
+               
+       TEST    
+               
+               
+               
+               
+               
+               
+               ";
+                var challenge = challengeCreator.Convert(board);
+                challenge.RackOfTiles = new Rack {Tiles = new List<char> {'I', 'N', 'G'}};
+
+                var solution = solver.Solve(challenge, Strategy.MaxPointsFirstWordThenQuit);
+
+                //make sure our solution is complete
+                solution.IsComplete.Should().Be(true);
+
+                solution.PlayedWords.Count.Should().Be(1);
+                solution.PointsSoFar.Should().Be(20);
+
+                var printedSolution = solutionPrinter.Print(solution).ToList();
+                printedSolution.Count().Should().Be(1);
+                printedSolution[0].Should().Be(@"Play TESTING for 20 point(s):
+
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+              T E S T I N G   
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+".RemoveCarriageReturns());
+            }
+            finally
+            {
+                container.Dispose();
+            }
+        }
+    }
+}
---
+++ src/WordsWithBrutes/DependencyInjection/WindsorInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using WordsWithBrutes.Components;
+using WordsWithBrutes.Components.Impl;
+
+namespace WordsWithBrutes.DependencyInjection
+{
+    /// <summary>
+    /// Helper class to register all our components in the container
+    /// </summary>
+    public class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            //register all our components here
+            container.Register(Component.For<ISolveChallenges>().ImplementedBy<SolveChallenges>());
+            container.Register(Component.For<IDetermineIfAStringIsAPlayableWord>().ImplementedBy<DetermineIfAStringIsAPlayableWord>());
+            container.Register(Component.For<IDeterminePotentialPlayLocationsForNextWord>().ImplementedBy<DeterminePotentialPlayLocationsForNextWord>());
+            container.Register(Component.For<IDetermineTheTotalPointsAndWordsCreatedByAPlay>().ImplementedBy<DetermineTheTotalPointsAndWordsCreatedByAPlay>());
+            container.Register(Component.For<IGenerateWordsToTryForPotentialPlayLocation>().ImplementedBy<GenerateWordsToTryForPotentialPlayLocation>());
+            container.Register(Component.For<IConvertGameStateToHumanReadableStrings>().ImplementedBy<ConvertGameStateToHumanReadableStrings>());
+            container.Register(Component.For<ITransformGameStateIntoTwoDimensionalArray>().ImplementedBy<TransformGameStateIntoTwoDimensionalArray>());
+            container.Register(Component.For<IGenerateStringPermutations>().ImplementedBy<GenerateStringPermutations>());
+            container.Register(Component.For<IConvertStringToWordsWithFriendsChallenge>().ImplementedBy<ConvertStringToWordsWithFriendsChallenge>());
+            container.Register(Component.For<IPopulateStandardWordsWithFriendsChallenge>().ImplementedBy<PopulateStandardWordsWithFriendsChallenge>());
+        }
+    }
+}
