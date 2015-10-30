7e490a4b2193a7d4669c6d778f8d3a4deb0c414b
Andrew Lane <andrew.lane@hobsons.com>
2015-10-30 16:04:54 -0400

    * Add an implementation for ISolveChallenges (SolveChallenges) and the start of some unit tests for it

---
+++ src/WordsWithBrutes.Tests/Components/Impl/TestSolveChallenges.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WordsWithBrutes.Components;
+using WordsWithBrutes.Components.Impl;
+using WordsWithBrutes.Enum;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Tests.Components.Impl
+{
+    /// <summary>
+    /// Tests the SolveChallenges class
+    /// </summary>
+    [TestFixture]
+    public class TestSolveChallenges
+    {
+        /// <summary>
+        /// Tests the QuickSolve strategy with an empty board and a single letter
+        /// </summary>
+        [Test]
+        public void TestQuickSolveStrategyWithEmptyBoardAndSingleLetter()
+        {
+            var dummyTiles = new List<char> { 'A' };
+            var dummyChallenge = new Challenge { StartingTiles = Enumerable.Empty<PlayedTile>(), MaxRackLength = 7, RackOfTiles = new Rack { Tiles = dummyTiles } };
+            var dummyPlayLocation = new PotentialPlayLocation { TileLocations = new List<TileLocation> { new TileLocation { X = 0, Y = 0 } } };
+            var dummyPlayedWord = new PlayedWord { TilesPlayed = new List<PlayedTile> { new PlayedTile { Letter = 'A', WasBlank = false } } };
+            const string dumyWordCreated = "BLAH";
+
+            var wordCheckerMock = new Mock<IDetermineIfAStringIsAPlayableWord>(MockBehavior.Strict);
+            var potentialPlayLocatorMock = new Mock<IDeterminePotentialPlayLocationsForNextWord>(MockBehavior.Strict);
+            var playAnalyzerMock = new Mock<IDetermineTheTotalPointsAndWordsCreatedByAPlay>(MockBehavior.Strict);
+            var wordGeneratorForPotentialPlayLocationMock = new Mock<IGenerateWordsToTryForPotentialPlayLocation>(MockBehavior.Strict);
+            var convertGameStateToHumanReadableStringsMock = new Mock<IConvertGameStateToHumanReadableStrings>(MockBehavior.Strict);
+
+            //make all words valid words
+            wordCheckerMock.Setup(mock => mock.IsAWord(It.IsAny<string>())).Returns(true);
+
+            //make the only potential play be the upper left-hand corner
+            potentialPlayLocatorMock.Setup(mock => mock.GetPlacesToPlayNextWord(It.IsAny<GameState>()))
+                .Returns(new List<PotentialPlayLocation> { dummyPlayLocation });
+
+            wordGeneratorForPotentialPlayLocationMock.Setup(mock => mock.Generate(It.IsAny<GameState>(), dummyPlayLocation, It.IsAny<char[]>()))
+                .Returns(new List<PlayedWord> { dummyPlayedWord });
+
+            playAnalyzerMock.Setup(mock => mock.GetPlayedWordsAndTotalPoints(It.IsAny<GameState>(), dummyPlayedWord.TilesPlayed))
+                .Returns(new WordsPlayedAndPointsScored
+                {
+                    TotalPointsAwarded = 5,
+                    WordsPlayed = new List<String> { dumyWordCreated }
+                });
+
+            convertGameStateToHumanReadableStringsMock.Setup(mock => mock.Print(It.IsAny<GameState>())).Returns(new[] { "some human readable string" });
+
+            var objectUnderTest = new SolveChallenges(wordCheckerMock.Object, potentialPlayLocatorMock.Object, playAnalyzerMock.Object,
+                wordGeneratorForPotentialPlayLocationMock.Object, convertGameStateToHumanReadableStringsMock.Object);
+            var solution = objectUnderTest.Solve(dummyChallenge, Strategy.QuickSolve);
+            solution.IsComplete.Should().BeTrue();
+            solution.PointsSoFar.Should().Be(5);
+            var playedWords = solution.PlayedWords.ToList();
+            playedWords.Count().Should().Be(1);
+            playedWords[0].Should().Be(dummyPlayedWord);
+        }
+    }
+}
---
+++ src/WordsWithBrutes/Components/Impl/SolveChallenges.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using log4net;
+using WordsWithBrutes.Constants;
+using WordsWithBrutes.Enum;
+using WordsWithBrutes.Model;
+
+namespace WordsWithBrutes.Components.Impl
+{
+    internal class SolveChallenges : ISolveChallenges
+    {
+        private readonly IDetermineIfAStringIsAPlayableWord _wordChecker;
+        private readonly IDeterminePotentialPlayLocationsForNextWord _potentialPlayLocator;
+        private readonly IDetermineTheTotalPointsAndWordsCreatedByAPlay _playAnalyzer;
+        private readonly IGenerateWordsToTryForPotentialPlayLocation _wordGeneratorForPotentialPlayLocation;
+        private readonly IConvertGameStateToHumanReadableStrings _gameStateToHumanReadableStringsConverter;
+
+        private static readonly ILog Logger = LogManager.GetLogger(typeof (SolveChallenges));
+
+        public SolveChallenges(IDetermineIfAStringIsAPlayableWord wordChecker, IDeterminePotentialPlayLocationsForNextWord potentialPlayLocator,
+            IDetermineTheTotalPointsAndWordsCreatedByAPlay playAnalyzer, IGenerateWordsToTryForPotentialPlayLocation wordGeneratorForPotentialPlayLocation,
+            IConvertGameStateToHumanReadableStrings gameStateToHumanReadableStringsConverter)
+        {
+            if (wordChecker == null) throw new ArgumentNullException("wordChecker");
+            if (potentialPlayLocator == null) throw new ArgumentNullException("potentialPlayLocator");
+            if (playAnalyzer == null) throw new ArgumentNullException("playAnalyzer");
+            if (wordGeneratorForPotentialPlayLocation == null) throw new ArgumentNullException("wordGeneratorForPotentialPlayLocation");
+            if (gameStateToHumanReadableStringsConverter == null) throw new ArgumentNullException("gameStateToHumanReadableStringsConverter");
+            _wordChecker = wordChecker;
+            _potentialPlayLocator = potentialPlayLocator;
+            _playAnalyzer = playAnalyzer;
+            _wordGeneratorForPotentialPlayLocation = wordGeneratorForPotentialPlayLocation;
+            _gameStateToHumanReadableStringsConverter = gameStateToHumanReadableStringsConverter;
+        }
+
+        /// <summary>
+        /// Solves the given challenge using the given strategy and returns the GameState of what it took to get there
+        /// </summary>
+        public GameState Solve(Challenge challenge, Strategy strategy)
+        {
+            if (Logger.IsDebugEnabled)
+            {
+                Logger.Debug("Starting to Solve");
+            }
+
+            //initialize our game state
+            var gameState = new GameState
+            {
+                Challenge = challenge,
+                CurrentRack = challenge.RackOfTiles,
+                IsComplete = false,
+                PlayedWords = new List<PlayedWord>(),
+                PointsSoFar = 0
+            };
+
+            return Iterate(gameState, strategy);
+        }
+
+        /// <summary>
+        /// The main guts of our brute force algorithm
+        /// </summary>
+        private GameState Iterate(GameState gameState, Strategy strategy, int depth = 0)
+        {
+            if (gameState.IsComplete) return gameState;
+
+            var wordsToPlay = _potentialPlayLocator.GetPlacesToPlayNextWord(gameState).ToList();
+
+            if (Logger.IsDebugEnabled)
+            {
+                Logger.Debug(String.Format("Found {0} places to potentially play next word", wordsToPlay.Count()));
+            }
+
+            int maxPointsForThisTurn = 0;
+            int maxTilesUsedThisTurn = 0;
+            PlayedWord bestWordThisTurn = null;
+
+            //for the MaxTotalPoints strategy, we'll keep track of all valid words and then loop through them to try them all
+            var allTheValidWordsToPursue = new Dictionary<PlayedWord, WordsPlayedAndPointsScored>();
+
+            foreach (var play in wordsToPlay)
+            {
+                var wordsToTryHere = _wordGeneratorForPotentialPlayLocation.Generate(gameState, play, 
+                    gameState.CurrentRack.Tiles.Take(gameState.Challenge.MaxRackLength).ToArray()).ToList();
+
+                if (Logger.IsDebugEnabled)
+                {
+                    Logger.Debug(String.Format("Found {0} words to try at this potential play location", wordsToTryHere.Count()));
+                }
+
+                foreach (var wordToTry in wordsToTryHere)
+                {
+                    var wordsAndPoints = _playAnalyzer.GetPlayedWordsAndTotalPoints(gameState, wordToTry.TilesPlayed);
+
+                    //if any of the words formed isn't in the dictionary, we'll move on to the next potential play
+                    if (wordsAndPoints.WordsPlayed.All(playedWord => _wordChecker.IsAWord(playedWord)) == false) continue;
+
+                    if (Logger.IsDebugEnabled)
+                    {
+                        Logger.Debug("Found a valid play");
+                    }
+
+                    //at this point we have a valid play
+                    switch (strategy)
+                    {
+                        //just use this word and move on
+                        case Strategy.QuickSolve:
+                            AddWordToGameState(gameState, wordToTry, wordsAndPoints.TotalPointsAwarded);
+                            return Iterate(gameState, strategy, depth + 1);
+
+                        case Strategy.MaxPointsEachWord:
+                        case Strategy.MaxPointsFirstWordThenQuit:
+                            if (bestWordThisTurn == null || wordsAndPoints.TotalPointsAwarded > maxPointsForThisTurn)
+                            {
+                                bestWordThisTurn = wordToTry;
+                                maxPointsForThisTurn = wordsAndPoints.TotalPointsAwarded;
+                            }
+                            break;
+
+                        case Strategy.MaxTilesUsedInFirstWordThenQuit:
+                            //check for a word of equal length to our max but more points
+                            if (bestWordThisTurn != null && wordToTry.TilesPlayed.Count() == maxTilesUsedThisTurn &&
+                                wordsAndPoints.TotalPointsAwarded > maxPointsForThisTurn)
+                            {
+                                bestWordThisTurn = wordToTry;
+                                maxPointsForThisTurn = wordsAndPoints.TotalPointsAwarded;                                
+                            }
+                            else if (bestWordThisTurn == null || wordToTry.TilesPlayed.Count() > maxTilesUsedThisTurn)
+                            {
+                                bestWordThisTurn = wordToTry;
+                                maxTilesUsedThisTurn = wordToTry.TilesPlayed.Count();
+                                maxPointsForThisTurn = wordsAndPoints.TotalPointsAwarded;
+                            }
+                            break;
+
+                        case Strategy.MaxTotalPoints:
+                            allTheValidWordsToPursue.Add(wordToTry, wordsAndPoints);
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+
+            switch (strategy)
+            {
+                case Strategy.MaxPointsEachWord:
+                    if (bestWordThisTurn != null)
+                    {
+                        AddWordToGameState(gameState, bestWordThisTurn, maxPointsForThisTurn);
+                        return Iterate(gameState, strategy, depth + 1);
+                    }
+                    break;
+
+                case Strategy.MaxPointsFirstWordThenQuit:
+                case Strategy.MaxTilesUsedInFirstWordThenQuit:
+                    if (bestWordThisTurn != null)
+                    {
+                        AddWordToGameState(gameState, bestWordThisTurn, maxPointsForThisTurn);
+                        gameState.IsComplete = true;
+                        return gameState;
+                    }
+                    break;
+
+                case Strategy.MaxTotalPoints:
+                    var allTheValidWordsToPursueCount = allTheValidWordsToPursue.Count();
+
+                    if (allTheValidWordsToPursueCount == 1)
+                    {
+                        //we've only got one valid play to pursue, so let's do that
+                        var validWordAndPoints = allTheValidWordsToPursue.First();
+                        AddWordToGameState(gameState, validWordAndPoints.Key, validWordAndPoints.Value.TotalPointsAwarded);
+                        return Iterate(gameState, strategy, depth + 1);
+                    }
+                    if (allTheValidWordsToPursueCount > 1)
+                    {
+                        var maxPointsOfAllTheChoices = 0;
+                        GameState gameStateOfMaxPoints = null;
+                        int counter = 0;
+                        foreach (var validWord in allTheValidWordsToPursue.Keys)
+                        {
+                            counter++;
+                            //clone the game state and use it to iterate
+                            var clonedGameState = gameState.Clone();
+                            AddWordToGameState(clonedGameState, validWord, allTheValidWordsToPursue[validWord].TotalPointsAwarded);
+                            var bestSolutionWithThisWord = Iterate(clonedGameState, strategy, depth + 1);
+                            if (gameStateOfMaxPoints == null || bestSolutionWithThisWord.PointsSoFar > maxPointsOfAllTheChoices)
+                            {
+                                //we've found our new winner
+                                gameStateOfMaxPoints = bestSolutionWithThisWord;
+                                maxPointsOfAllTheChoices = bestSolutionWithThisWord.PointsSoFar;
+
+                                if (depth == 0)
+                                {
+                                    if (Logger.IsInfoEnabled)
+                                    {
+                                        var start = _gameStateToHumanReadableStringsConverter.Print(clonedGameState);
+                                        var completion = _gameStateToHumanReadableStringsConverter.Print(gameStateOfMaxPoints);
+
+                                        Logger.Info(
+                                            String.Format(@"The best solution (so far) that started with this game state resulted in {0} points.
+
+Start: 
+{1}
+**********
+
+Finish: 
+{2}",
+                                                bestSolutionWithThisWord.PointsSoFar, start.First(), String.Join(Environment.NewLine, completion)));
+                                        Logger.Info("-------------------------");
+                                    }
+                                }
+
+                            }
+                            if (depth == 0)
+                            {
+                                if (Logger.IsInfoEnabled)
+                                {
+                                    Logger.Info(String.Format("Done with {0}/{1}", counter, allTheValidWordsToPursue.Count));
+                                }
+                            }
+                        }
+                        return gameStateOfMaxPoints;
+                    }
+                    break;
+                    case Strategy.QuickSolve:
+                        //nothing to do
+                        break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+
+            //if we get here, we're done
+            gameState.IsComplete = true;
+            return gameState;
+        }
+
+        /// <summary>
+        /// Helper method to transition the state of the GameState object
+        /// </summary>
+        private static void AddWordToGameState(GameState gameState, PlayedWord playedWord, int pointsAwarded)
+        {
+            //add the points to our tally
+            gameState.PointsSoFar += pointsAwarded;
+
+            //add the played word
+            gameState.PlayedWords.Add(playedWord);
+
+            //remove the played tiles from the rack
+            gameState.CurrentRack = TransitionRackAfterPlayedWord(gameState.CurrentRack, playedWord);
+
+            //check to see if we're done if we played all our tiles
+            if (gameState.CurrentRack.Tiles.Any() == false)
+            {
+                gameState.IsComplete = true;
+            }
+        }
+
+        /// <summary>
+        /// Helper function to remove the played tiles from the rack
+        /// </summary>
+        private static Rack TransitionRackAfterPlayedWord(Rack rack, PlayedWord playedWord)
+        {
+            foreach (var tile in playedWord.TilesPlayed)
+            {
+                rack.Tiles.RemoveAt(rack.Tiles.IndexOf(tile.WasBlank ? WordsWithBrutesConstants.BlankTile : tile.Letter));
+            }
+            return rack;
+        }
+    }
+}
