6f52cece4f5ef8fbc043a3c57f983f48c303e4c0
Andrew Lane <andrew.lane@hobsons.com>
2015-11-10 14:04:30 -0500

    Add a Solver test for my best word ever in real life

--- src/WordsWithBrutes.Tests/IntegrationTests/SolverTests.cs
+++ src/WordsWithBrutes.Tests/IntegrationTests/SolverTests.cs
@@ -156,5 +156,76 @@ D R O O p I E R
             }
         }
 
+        /// <summary>
+        /// Verifies the system will find the maximum play of MOUTHERS in a certain board.
+        /// </summary>
+        [Test]
+        public void MouthersTest()
+        {
+            var container = new WindsorContainer();
+            try
+            {
+                container.Install(new WindsorInstaller());
+
+                var challengeCreator = container.Resolve<IConvertStringToWordsWithFriendsChallenge>();
+                var solutionPrinter = container.Resolve<IConvertGameStateToHumanReadableStrings>();
+                var solver = container.Resolve<ISolveChallenges>();
+
+                const string board = @"
+               
+               
+               
+               
+           C   
+     B     A   
+     O     M   
+     ALT   P   
+   OUT WAITS   
+       E FI    
+      AE       
+     GET       
+     A         
+     L         
+               
+";
+                var challenge = challengeCreator.Convert(board);
+                challenge.RackOfTiles = new Rack { Tiles = new List<char> { 'M','U','T','H','E','R','S' } };
+
+                var solution = solver.Solve(challenge, Strategy.MaxPointsFirstWordThenQuit);
+
+                //make sure our solution is complete
+                solution.IsComplete.Should().Be(true);
+
+                solution.PlayedWords.Count.Should().Be(1);
+                solution.PointsSoFar.Should().Be(155);
+
+                var printedSolution = solutionPrinter.Print(solution).ToList();
+                printedSolution.Count().Should().Be(1);
+                printedSolution[0].Should().Be(@"Play MOUTHERS for 155 point(s):
+
+                              
+                              
+                              
+                              
+                      C       
+          B           A       
+          O           M       
+      M   A L T       P       
+      O U T   W A I T S       
+      U       E   F I         
+      T     A E               
+      H   G E T               
+      E   A                   
+      R   L                   
+      S                       
+".RemoveCarriageReturns());
+            }
+            finally
+            {
+                container.Dispose();
+            }
+        }
+
+
     }
 }
